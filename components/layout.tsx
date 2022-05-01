ff135d9854635d5e9c849d6b0e13a424e2731336
harshithpabbati <pabbatiharshith@gmail.com>
2022-05-01 09:41:43 +0530

    Make it a bit responsive for smaller screens

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,25 +5,40 @@ import Settings from './settings';
 import RtmpUrlModal from './rtmpUrlModal';
 import Sidebar from './sidebar';
 import LayoutHeader from './PaneHeader';
+import { useWindowSize } from '../hooks/useWindowSize';
 
 const Layout = () => {
   const { callRef, joinedMeeting } = useCall();
   const [show, setShow] = useState(false);
+  const { width } = useWindowSize();
+
+  const getCallWidth = () => {
+    switch (width) {
+      case width >= 1200:
+        return 75;
+      case width < 1200:
+        return 60;
+    }
+  };
 
   return (
     <Pane display="flex" height="100vh" overflow="hidden">
-      <Pane width={joinedMeeting ? '75%' : '100%'}>
+      <Pane width={joinedMeeting ? `${getCallWidth()}%` : '100%'}>
         <div ref={callRef} />
       </Pane>
       {joinedMeeting && (
-        <Pane width="25%" background="tint1" minHeight="100vh">
+        <Pane
+          width={`${100 - getCallWidth()}%`}
+          background="tint1"
+          minHeight="100vh"
+        >
           <LayoutHeader />
           <Pane width="100%" display="flex">
             <Pane width="85%">
               <Settings startStreaming={setShow} />
             </Pane>
             <Pane
-              minWidth="15%"
+              width="15%"
               background="white"
               padding={10}
               minHeight="100vh"
