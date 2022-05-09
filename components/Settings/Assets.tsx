5fd42bcd37087be3426e28f0a34ef5ccf67d65ac
harshithpabbati <pabbatiharshith@gmail.com>
2022-05-09 16:07:56 +0530

    update the image asset section

--- components/Settings/Assets.tsx
+++ components/Settings/Assets.tsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import {
-  Pane,
-  TextInputField,
-  Card,
   Button,
+  Card,
+  FileCard,
+  FileUploader,
   Heading,
+  MimeType,
+  Pane,
   Paragraph,
+  TextInputField,
 } from 'evergreen-ui';
 import { useVCS } from '../../contexts/VCSProvider';
 
@@ -13,17 +16,32 @@ const Asset = () => {
   const { assets, setAssets } = useVCS();
   const [values, setValues] = useState({
     name: '',
-    url: '',
+    file: [],
   });
+
+  const handleChangeFile = useCallback((files: any) => {
+    setValues(values => ({ ...values, name: files[0].name, file: [files[0]] }));
+  }, []);
+  const handleRemove = useCallback(() => {
+    setValues(values => ({ ...values, file: [] }));
+  }, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setValues(values => ({ ...values, [e.target.name]: e.target.value }));
 
-  const handleSaveAsset = () =>
-    setAssets({ ...assets, [values.name]: values.url });
+  const handleSaveAsset = () => {
+    const reader = new FileReader();
+    reader.onload = function (readEv) {
+      // @ts-ignore
+      setAssets({ ...assets, [values.name]: readEv.target.result });
+      setValues({ name: '', file: [] });
+    };
+    reader.readAsDataURL(values.file[0]);
+  };
 
   return (
     <Card background="white" padding={24} marginBottom={16}>
+      <Heading marginBottom={16}>Create an asset?</Heading>
       <TextInputField
         name="name"
         label="Asset Name"
@@ -31,23 +49,37 @@ const Asset = () => {
         onChange={handleChange}
         marginBottom={20}
       />
-      <TextInputField
-        name="url"
-        label="Asset URL"
-        value={values.url}
-        onChange={handleChange}
-        marginBottom={20}
+      <FileUploader
+        label="Upload File"
+        description="You can upload 1 file. File can be up to 1 MB."
+        maxSizeInBytes={1024 ** 2}
+        maxFiles={1}
+        onChange={handleChangeFile}
+        acceptedMimeTypes={[MimeType.png]}
+        renderFile={file => {
+          const { name, size, type } = file;
+          return (
+            <FileCard
+              key={name}
+              name={name}
+              onRemove={handleRemove}
+              sizeInBytes={size}
+              type={type}
+            />
+          );
+        }}
+        values={values.file}
       />
-      <Button onClick={handleSaveAsset}>Save Asset</Button>
+      <Button onClick={handleSaveAsset} width="100%" appearance="primary">
+        Save Asset
+      </Button>
     </Card>
   );
 };
 
 const AssetSettings = () => {
   const { assets, setAssets } = useVCS();
-  const [createAsset, setCreateAsset] = useState(false);
 
-  const handleAddAsset = () => setCreateAsset(create => !create);
   const handleRemoveAsset = (key: string) => {
     const newAssets = assets;
     delete newAssets[key];
@@ -57,28 +89,22 @@ const AssetSettings = () => {
   return (
     <Pane>
       <Pane>
+        <Asset />
         <Heading>Session Assets ({Object.keys(assets).length})</Heading>
-
         <Pane marginY={10}>
           {Object.keys(assets).map(asset => (
-            <Card key={asset} background="white" padding={20} marginY={8}>
-              <Heading>{asset}</Heading>
-              <Paragraph marginY={5}>{assets[asset]}</Paragraph>
-              <Button
-                intent="danger"
-                marginY={10}
-                onClick={() => handleRemoveAsset(asset)}
-              >
-                Remove
-              </Button>
-            </Card>
+            <FileCard
+              key={asset}
+              name={asset}
+              type={MimeType.png}
+              sizeInBytes={Math.round(
+                ((assets[asset].length - 'data:image/png;base64,'.length) * 3) /
+                  4,
+              )}
+              onRemove={() => handleRemoveAsset(asset)}
+            />
           ))}
         </Pane>
-
-        {createAsset && <Asset />}
-        <Button onClick={handleAddAsset}>
-          {createAsset ? 'Close' : 'Add New Asset'}
-        </Button>
       </Pane>
     </Pane>
   );
