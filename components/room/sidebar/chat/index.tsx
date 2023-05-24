373b04b18ed7f1d457c468a3b822b87a35562832
harshithpabbati <pabbatiharshith@gmail.com>
2023-05-24 16:48:21 +0530

    add empty state in chat

--- components/room/sidebar/chat/index.tsx
+++ components/room/sidebar/chat/index.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useMessages } from '@/states/messagesState';
 
+import { Icons } from '@/components/icons';
 import { ChatInput } from '@/components/room/sidebar/chat/ChatInput';
 import { ChatMessage } from '@/components/room/sidebar/chat/ChatMessage';
 
@@ -19,15 +20,29 @@ export function Chat() {
     <div className="relative flex h-full flex-col justify-between p-4">
       <div
         ref={messagesRef}
-        className="flex max-h-[calc(100%-4rem)] flex-col overflow-y-auto"
+        className="flex h-full max-h-[calc(100%-4rem)] flex-col overflow-y-auto"
       >
-        {messages.map((message, index) => (
-          <ChatMessage
-            message={message}
-            sameSender={message.fromId === messages?.[index - 1]?.fromId}
-            key={message.id}
-          />
-        ))}
+        {messages.length > 0 ? (
+          messages.map((message, index) => (
+            <ChatMessage
+              message={message}
+              sameSender={message.fromId === messages?.[index - 1]?.fromId}
+              key={message.id}
+            />
+          ))
+        ) : (
+          <div className="flex h-full items-center justify-center">
+            <div className="flex flex-col items-center justify-center gap-y-4 rounded-md bg-muted p-8">
+              <div className="w-30 h-30 rounded-full bg-background p-4">
+                <Icons.chat className="h-5 w-5" />
+              </div>
+              <h3 className="text-sm">It&apos;s empty in here!</h3>
+              <p className="text-xs">
+                Would you like to say something to the group?
+              </p>
+            </div>
+          </div>
+        )}
       </div>
       <ChatInput />
     </div>
