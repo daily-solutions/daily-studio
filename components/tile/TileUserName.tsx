459437aac6a32aa7ec0fdf07c07b4967b0e34358
harshithpabbati <pabbatiharshith@gmail.com>
2023-06-04 16:34:45 +0530

    add mic icon in tile

--- components/tile/TileUserName.tsx
+++ components/tile/TileUserName.tsx
@@ -1,15 +1,23 @@
 import { useMemo } from 'react';
 import { useParticipantProperty } from '@daily-co/daily-react';
 
+import { cn } from '@/lib/utils';
+import { Icon } from '@/components/icons';
+
 interface Props {
   sessionId: string;
 }
 
+const AUDIO_OFF = ['off', 'interrupted', 'blocked'];
+
 export function TileUserName({ sessionId }: Props) {
-  const [userName, isLocal, participantType] = useParticipantProperty(
-    sessionId,
-    ['user_name', 'local', 'participantType']
-  );
+  const [userName, isLocal, participantType, audioState] =
+    useParticipantProperty(sessionId, [
+      'user_name',
+      'local',
+      'participantType',
+      'tracks.audio.state',
+    ]);
 
   const tileName = useMemo(
     () =>
@@ -18,7 +26,16 @@ export function TileUserName({ sessionId }: Props) {
   );
 
   return (
-    <div className="absolute bottom-2 left-2 rounded-md bg-muted p-2">
+    <div className="absolute bottom-2 left-2 flex items-center justify-center rounded-md bg-muted p-2">
+      {participantType !== 'remote-media-player' && (
+        <Icon
+          icon={AUDIO_OFF.includes(audioState) ? 'micOff' : 'micOn'}
+          className={cn(
+            'mr-2 h-4 w-4',
+            AUDIO_OFF.includes(audioState) ? 'text-destructive' : 'text-inherit'
+          )}
+        />
+      )}
       <p className="select-none text-sm">
         {tileName} {isLocal && '(You)'}
       </p>
