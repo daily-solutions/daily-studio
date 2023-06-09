65ac8cd5c23f00a85725251027f45f0a1da73bdd
harshithpabbati <pabbatiharshith@gmail.com>
2023-06-09 20:22:32 +0530

    avoid sending higher layers if cpu load is high

--- components/room/listeners/SendSettingsListener.tsx
+++ components/room/listeners/SendSettingsListener.tsx
@@ -1,6 +1,9 @@
 import { useCallback } from 'react';
 import { useSendQualityState } from '@/states/sendQualityState';
-import { DailyEventObjectNetworkQualityEvent } from '@daily-co/daily-js';
+import {
+  DailyEventObjectCpuLoadEvent,
+  DailyEventObjectNetworkQualityEvent,
+} from '@daily-co/daily-js';
 import { useDailyEvent } from '@daily-co/daily-react';
 
 import { useSendSettingsQuality } from '@/hooks/useSendSettings';
@@ -9,33 +12,31 @@ export function SendSettingsListener() {
   const [defaultQuality] = useSendQualityState();
   const { updateQuality } = useSendSettingsQuality();
 
-  useDailyEvent(
-    'network-quality-change',
-    useCallback(
-      async (ev: DailyEventObjectNetworkQualityEvent) => {
-        if (defaultQuality !== 'auto') return;
+  const handleNetworkQualityChange = useCallback(
+    async (ev: DailyEventObjectNetworkQualityEvent) => {
+      if (defaultQuality !== 'auto') return;
 
-        const { threshold } = ev;
-        switch (threshold) {
-          case 'very-low':
-            await updateQuality('low');
-            break;
-          case 'low':
-            await updateQuality('medium');
-            break;
-          case 'good':
-            await updateQuality('high');
-            break;
-        }
-      },
-      [defaultQuality, updateQuality]
-    )
+      const { threshold } = ev;
+      switch (threshold) {
+        case 'very-low':
+          await updateQuality('low');
+          break;
+        case 'low':
+          await updateQuality('medium');
+          break;
+        case 'good':
+          await updateQuality('high');
+          break;
+      }
+    },
+    [defaultQuality, updateQuality]
   );
 
   // disables sending high quality video when screen sharing
-  const handleSendQualityOnScreenShare = useCallback(
+  const handleOnScreenShare = useCallback(
     async (ev) => {
       if (defaultQuality !== 'auto') return;
+
       if (ev.action === 'local-screen-share-started')
         await updateQuality('medium');
       else await updateQuality('high');
@@ -43,8 +44,32 @@ export function SendSettingsListener() {
     [defaultQuality, updateQuality]
   );
 
-  useDailyEvent('local-screen-share-started', handleSendQualityOnScreenShare);
-  useDailyEvent('local-screen-share-stopped', handleSendQualityOnScreenShare);
+  const handleCPULoadChange = useCallback(
+    async (ev: DailyEventObjectCpuLoadEvent) => {
+      if (defaultQuality !== 'auto') return;
+
+      const { cpuLoadState, cpuLoadStateReason } = ev;
+
+      switch (cpuLoadState) {
+        case 'high':
+          await updateQuality(
+            cpuLoadStateReason === 'encode' ? 'low' : 'medium'
+          );
+          break;
+        case 'low':
+          await updateQuality('high');
+          break;
+        default:
+          break;
+      }
+    },
+    [defaultQuality, updateQuality]
+  );
+
+  useDailyEvent('cpu-load-change', handleCPULoadChange);
+  useDailyEvent('network-quality-change', handleNetworkQualityChange);
+  useDailyEvent('local-screen-share-started', handleOnScreenShare);
+  useDailyEvent('local-screen-share-stopped', handleOnScreenShare);
 
   return null;
 }
