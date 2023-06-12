4f5dde33e812d5b47049ddb303f03cbfded51f0e
harshithpabbati <pabbatiharshith@gmail.com>
2023-06-12 20:43:52 +0530

    allow owners to mute cam & mic of other participants

--- components/room/sidebar/people/ParticipantMenu.tsx
+++ components/room/sidebar/people/ParticipantMenu.tsx
@@ -25,18 +25,29 @@ interface Props {
   variant?: ButtonProps['variant'];
 }
 
+const IS_OFF = ['off', 'blocked'];
+
 export function ParticipantMenu({ sessionId, variant = 'ghost' }: Props) {
   const daily = useDaily();
   const localSessionId = useLocalSessionId();
   const isLocalOwner = useParticipantProperty(localSessionId, 'owner');
-  const [isOwner, isLocal, userData, hasPresence, participantType] =
-    useParticipantProperty(sessionId, [
-      'owner',
-      'local',
-      'userData',
-      'permissions.hasPresence',
-      'participantType',
-    ]);
+  const [
+    isOwner,
+    isLocal,
+    userData,
+    hasPresence,
+    participantType,
+    video,
+    audio,
+  ] = useParticipantProperty(sessionId, [
+    'owner',
+    'local',
+    'userData',
+    'permissions.hasPresence',
+    'participantType',
+    'tracks.video.state',
+    'tracks.audio.state',
+  ]);
 
   const { canSendAudio, canSendVideo, canSendScreenVideo } =
     useRemotePermissions(sessionId);
@@ -101,6 +112,18 @@ export function ParticipantMenu({ sessionId, variant = 'ghost' }: Props) {
     else await updateRemoteMediaPlayer({ state: 'play' });
   }, [isPlaying, updateRemoteMediaPlayer]);
 
+  const handleTurnOff = useCallback(
+    (device: 'audio' | 'video' = 'video') => {
+      if (!daily) return;
+
+      daily.updateParticipant(
+        sessionId,
+        device === 'audio' ? { setAudio: false } : { setVideo: false }
+      );
+    },
+    [daily, sessionId]
+  );
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -110,58 +133,7 @@ export function ParticipantMenu({ sessionId, variant = 'ghost' }: Props) {
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>Actions</DropdownMenuLabel>
-        {participantType !== 'remote-media-player' && (
-          <>
-            <DropdownMenuSeparator />
-            <DropdownMenuCheckboxItem
-              disabled={
-                !(userData?.['acceptedToJoin'] || userData?.['onStage'])
-              }
-              checked={hasPresence && userData?.['onStage']}
-              onCheckedChange={() =>
-                setStageVisibility(sessionId, !userData?.['onStage'])
-              }
-            >
-              Visible on stream
-            </DropdownMenuCheckboxItem>
-          </>
-        )}
-        {isLocalOwner &&
-          !isOwner &&
-          !isLocal &&
-          participantType !== 'remote-media-player' && (
-            <>
-              <DropdownMenuSeparator />
-              <DropdownMenuLabel>Permissions</DropdownMenuLabel>
-              <DropdownMenuCheckboxItem
-                checked={canSendAudio}
-                onCheckedChange={(c) => handlePermissionChange('audio', c)}
-              >
-                Audio
-              </DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem
-                checked={canSendVideo}
-                onCheckedChange={(c) => handlePermissionChange('video', c)}
-              >
-                Video
-              </DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem
-                checked={canSendScreenVideo}
-                onCheckedChange={(c) => handlePermissionChange('screen', c)}
-              >
-                ScreenShare
-              </DropdownMenuCheckboxItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem
-                className="text-destructive"
-                onClick={handleRemoveFromStage}
-              >
-                <Icons.userMinus className="mr-2 h-4 w-4" />
-                <span>Remove from stage</span>
-              </DropdownMenuItem>
-            </>
-          )}
-        {participantType === 'remote-media-player' && (
+        {participantType === 'remote-media-player' ? (
           <>
             <DropdownMenuItem onClick={handlePlayPause}>
               {isPlaying ? (
@@ -179,6 +151,73 @@ export function ParticipantMenu({ sessionId, variant = 'ghost' }: Props) {
               <Icons.stop className="mr-2 h-4 w-4" />
               <span>Stop media player</span>
             </DropdownMenuItem>
+          </>
+        ) : (
+          <>
+            <DropdownMenuSeparator />
+            <DropdownMenuCheckboxItem
+              disabled={
+                !(userData?.['acceptedToJoin'] || userData?.['onStage'])
+              }
+              checked={hasPresence && userData?.['onStage']}
+              onCheckedChange={() =>
+                setStageVisibility(sessionId, !userData?.['onStage'])
+              }
+            >
+              Visible on stream
+            </DropdownMenuCheckboxItem>
+            {isLocalOwner && !isLocal && (
+              <>
+                <DropdownMenuSeparator />
+                <DropdownMenuLabel>Moderation</DropdownMenuLabel>
+                <DropdownMenuItem
+                  disabled={IS_OFF.includes(video)}
+                  onClick={() => handleTurnOff('video')}
+                >
+                  <Icons.videoOff className="mr-2 h-4 w-4" />
+                  <span>Turn off camera</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  disabled={IS_OFF.includes(audio)}
+                  onClick={() => handleTurnOff('audio')}
+                >
+                  <Icons.micOff className="mr-2 h-4 w-4" />
+                  <span>Mute microphone</span>
+                </DropdownMenuItem>
+              </>
+            )}
+            {isLocalOwner && !isOwner && !isLocal && (
+              <>
+                <DropdownMenuSeparator />
+                <DropdownMenuLabel>Permissions</DropdownMenuLabel>
+                <DropdownMenuCheckboxItem
+                  checked={canSendAudio}
+                  onCheckedChange={(c) => handlePermissionChange('audio', c)}
+                >
+                  Audio
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                  checked={canSendVideo}
+                  onCheckedChange={(c) => handlePermissionChange('video', c)}
+                >
+                  Video
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                  checked={canSendScreenVideo}
+                  onCheckedChange={(c) => handlePermissionChange('screen', c)}
+                >
+                  ScreenShare
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  className="text-destructive"
+                  onClick={handleRemoveFromStage}
+                >
+                  <Icons.userMinus className="mr-2 h-4 w-4" />
+                  <span>Remove from stage</span>
+                </DropdownMenuItem>
+              </>
+            )}
           </>
         )}
       </DropdownMenuContent>
