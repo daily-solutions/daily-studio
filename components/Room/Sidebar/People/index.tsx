a737c3340aa8e7b94e46f127226d8d812d83694e
harshithpabbati <pabbatiharshith@gmail.com>
2023-06-13 18:03:40 +0530

    minor fixes

--- components/Room/Sidebar/People/index.tsx
+++ components/Room/Sidebar/People/index.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useViewers } from '@/states/viewersState';
+import { DailyParticipant } from '@daily-co/daily-js';
 import { useParticipantIds } from '@daily-co/daily-react';
 
-import { useParticipants } from '@/hooks/useParticipants';
 import { Participant } from '@/components/Room/Sidebar/People/Participant';
 import { Viewer } from '@/components/Room/Sidebar/People/Viewer';
 
 export function People() {
-  const { participantIds } = useParticipants();
+  const participantIds = useParticipantIds({
+    filter: useCallback((p: DailyParticipant) => p.permissions.hasPresence, []),
+    sort: useCallback((a: DailyParticipant, b: DailyParticipant) => {
+      if (a.local || b.local) return 1;
+      if (a.user_name < b.user_name) return -1;
+      if (a.user_name > b.user_name) return 1;
+      return 0;
+    }, []),
+  });
 
   const [viewers] = useViewers();
 
@@ -15,7 +23,7 @@ export function People() {
     <div className="p-4">
       <p className="mb-3 text-xs text-muted-foreground">
         {participantIds.length}{' '}
-        {participantIds.length === 1 ? 'person' : 'people'} on stage
+        {participantIds.length === 1 ? 'person' : 'people'} on the call
       </p>
       <div className="flex flex-col gap-y-1">
         {participantIds.map((id) => (
--- components/Room/Stage/index.tsx
+++ components/Room/Stage/index.tsx
@@ -1,37 +1,31 @@
 import React, { useState } from 'react';
 import { Tabs, TabsContent, TabsList } from '@/ui/Tabs';
-import {
-  useLocalSessionId,
-  useParticipantProperty,
-  usePermissions,
-} from '@daily-co/daily-react';
+import { useLocalSessionId } from '@daily-co/daily-react';
 
+import { useIsOwner } from '@/hooks/useIsOwner';
 import { useParticipants } from '@/hooks/useParticipants';
+import { useStage } from '@/hooks/useStage';
 import { Participants } from '@/components/Room/Stage/Participants';
 import { TabHeader } from '@/components/Room/Stage/TabHeader';
 import { Tile } from '@/components/Tile';
 
 export function Stage() {
-  const [tab, setTab] = useState('stage');
-
+  const isOwner = useIsOwner();
+  const localSessionId = useLocalSessionId();
+  const { state } = useStage();
   const { participantIds, waitingParticipantIds } = useParticipants();
 
-  const localSessionId = useLocalSessionId();
-  const [isOwner, userData] = useParticipantProperty(localSessionId, [
-    'owner',
-    'userData',
-  ]);
-  const { hasPresence } = usePermissions();
+  const [tab, setTab] = useState('stage');
 
-  if (!hasPresence || (!isOwner && userData?.['onStage'])) return null;
-
-  if (!isOwner) {
+  if (state === 'back-stage') {
     return (
       <div className="absolute bottom-28 left-8 w-[calc(9rem*(16/9))]">
         <Tile sessionId={localSessionId} noVideoTileColor="bg-background" />
       </div>
     );
   }
+
+  if (!isOwner) return null;
 
   return (
     <Tabs
