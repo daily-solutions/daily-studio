c4434d7cc081a87f5337d52e32f0d1a23541bb0a
harshithpabbati <pabbatiharshith@gmail.com>
2023-06-19 17:05:01 +0530

    change VCS aspect ratio to 4:3 for mobile

--- components/Vcs/VcsPreview.tsx
+++ components/Vcs/VcsPreview.tsx
@@ -2,14 +2,19 @@ import React, { useRef } from 'react';
 import { AspectRatio } from '@/ui/AspectRatio';
 
 import { useAspectRatio } from '@/hooks/useAspectRatio';
+import { useIsMobile } from '@/hooks/useIsMobile';
 import { useVCS } from '@/hooks/useVCS';
 
+const MOBILE_ASPECT_RATIO = 4 / 3;
+const DESKTOP_ASPECT_RATIO = 16 / 9;
+
 export function VcsPreview() {
+  const isMobile = useIsMobile();
   const divRef = useRef<HTMLDivElement>(null);
 
   const size = useAspectRatio({
     ref: divRef,
-    aspectRatio: 16 / 9,
+    aspectRatio: isMobile ? MOBILE_ASPECT_RATIO : DESKTOP_ASPECT_RATIO,
   });
 
   const { outputElementRef } = useVCS({
@@ -22,7 +27,10 @@ export function VcsPreview() {
       className="flex h-full w-full flex-1 items-center justify-center bg-muted"
     >
       <div style={{ ...size }}>
-        <AspectRatio className="bg-black" ratio={16 / 9}>
+        <AspectRatio
+          className="bg-black"
+          ratio={isMobile ? MOBILE_ASPECT_RATIO : DESKTOP_ASPECT_RATIO}
+        >
           <div
             className="h-full w-full overflow-hidden"
             ref={outputElementRef}
