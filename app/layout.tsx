9fcf550c451bc70410594d222689680b0e6f2791
harshithpabbati <pabbatiharshith@gmail.com>
2023-06-30 14:50:24 +0530

    remove TailwindIndicator component

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import { siteConfig } from '@/config/site';
 import { fontSans } from '@/lib/fonts';
 import { cn } from '@/lib/utils';
 import { RecoilProvider } from '@/components/Providers';
-import { TailwindIndicator } from '@/components/TailwindIndicator';
 
 export const metadata: Metadata = {
   title: {
@@ -43,7 +42,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <RecoilProvider>
           <div className="h-full overflow-hidden">{children}</div>
           <Toaster />
-          <TailwindIndicator />
         </RecoilProvider>
       </body>
     </html>
