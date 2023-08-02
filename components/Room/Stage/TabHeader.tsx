f712a01615b774561295f07c98a3c020e0064f05
harshithpabbati <pabbatiharshith@gmail.com>
2023-08-02 23:08:17 +0530

    change active badge color

--- components/Room/Stage/TabHeader.tsx
+++ components/Room/Stage/TabHeader.tsx
@@ -13,7 +13,7 @@ export function TabHeader({ name, value, count, active }: Props) {
   return (
     <TabsTrigger value={value} className="w-20 rounded-md">
       <div className="flex flex-col items-center justify-center gap-2">
-        <Badge variant={active ? 'secondary' : 'default'}>{count}</Badge>
+        <Badge variant={active ? 'default' : 'outline'}>{count}</Badge>
         {name}
       </div>
     </TabsTrigger>
