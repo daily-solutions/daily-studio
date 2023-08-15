1e8479bdf5dc1f00e880725ae7a9f81858edd388
harshithpabbati <pabbatiharshith@gmail.com>
2023-08-15 20:28:08 +0530

    remove fixed height & width in VCSPreview

--- components/Vcs/VcsPreview.tsx
+++ components/Vcs/VcsPreview.tsx
@@ -3,16 +3,11 @@ import {
   DESKTOP_ASPECT_RATIO,
   MOBILE_ASPECT_RATIO,
 } from '@/constants/aspectRatio';
-import { usePermissions } from '@daily-co/daily-react';
 
-import { cn } from '@/lib/utils';
 import { useIsMobile } from '@/hooks/useIsMobile';
-import { useIsOwner } from '@/hooks/useIsOwner';
 import { useVCS } from '@/hooks/useVCS';
 
 export function VcsPreview() {
-  const { hasPresence } = usePermissions();
-  const isOwner = useIsOwner();
   const isMobile = useIsMobile();
   const divRef = useRef<HTMLDivElement>(null);
 
@@ -29,14 +24,7 @@ export function VcsPreview() {
   return (
     <div
       ref={divRef}
-      className={cn(
-        'flex w-full items-center justify-center bg-muted',
-        isOwner
-          ? 'h-[calc(100dvh-24rem)]'
-          : hasPresence
-          ? 'h-[calc(100dvh-19rem)]'
-          : 'h-[calc(100dvh-9rem)]',
-      )}
+      className="flex h-full w-full flex-1 items-center justify-center overflow-hidden bg-muted"
     >
       <div
         className="bg-black"
