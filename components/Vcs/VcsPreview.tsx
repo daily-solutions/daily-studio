15bdbb5e47aea5933f669d7a95a5a2541e1c4ac7
harshithpabbati <pabbatiharshith@gmail.com>
2023-08-30 15:20:29 +0530

    fix vcs preview when we resize the element

--- components/Vcs/VcsPreview.tsx
+++ components/Vcs/VcsPreview.tsx
@@ -2,26 +2,22 @@ import React, { useEffect, useMemo, useRef } from 'react';
 import {
   DESKTOP_ASPECT_RATIO,
   DESKTOP_PORTRAIT_ASPECT_RATIO,
-  MOBILE_ASPECT_RATIO,
 } from '@/constants/aspectRatio';
 import { useParams } from '@/states/params';
 
-import { useIsMobile } from '@/hooks/useIsMobile';
 import { useVCS } from '@/hooks/useVCS';
 
 export function VcsPreview() {
-  const isMobile = useIsMobile();
   const divRef = useRef<HTMLDivElement>(null);
   const [params] = useParams();
 
   const aspectRatio = useMemo(() => {
-    if (isMobile) return MOBILE_ASPECT_RATIO;
-    else if (params?.['custom.viewport'] === 'portrait')
+    if (params?.['custom.viewport'] === 'portrait')
       return DESKTOP_PORTRAIT_ASPECT_RATIO;
     else return DESKTOP_ASPECT_RATIO;
-  }, [isMobile, params]);
+  }, [params]);
 
-  const { outputElementRef, vcsCompRef } = useVCS({
+  const { vcsCompRef, vcsContainerRef } = useVCS({
     viewportRef: divRef,
     aspectRatio,
   });
@@ -34,9 +30,13 @@ export function VcsPreview() {
   return (
     <div
       ref={divRef}
-      className="flex h-full w-full flex-1 items-center justify-center overflow-hidden bg-muted"
+      className="LiveFeed relative flex h-full w-full items-center justify-center overflow-hidden bg-muted"
     >
-      <div className="bg-black" ref={outputElementRef} />
+      <div
+        ref={vcsContainerRef}
+        className="VCSRenderer bg-black"
+        style={{ aspectRatio }}
+      />
     </div>
   );
 }
