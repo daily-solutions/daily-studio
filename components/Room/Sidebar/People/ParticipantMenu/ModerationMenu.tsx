1c1618c00ff437f4ea9e323e69d00036693253a7
harshithpabbati <pabbatiharshith@gmail.com>
2023-10-07 02:10:56 +0530

    allow to stop remote user screenshare

--- components/Room/Sidebar/People/ParticipantMenu/ModerationMenu.tsx
+++ components/Room/Sidebar/People/ParticipantMenu/ModerationMenu.tsx
@@ -5,6 +5,7 @@ import {
   DropdownMenuSeparator,
 } from '@/ui/DropdownMenu';
 import { Icons } from '@/ui/Icons';
+import { DailyParticipantUpdateOptions } from '@daily-co/daily-js';
 import { useDaily, useParticipantProperty } from '@daily-co/daily-react';
 
 import { useIsOwner } from '@/hooks/useIsOwner';
@@ -15,20 +16,36 @@ export function ModerationMenu({ sessionId }: { sessionId: string }) {
   const daily = useDaily();
   const isOwner = useIsOwner();
 
-  const [video, audio] = useParticipantProperty(sessionId, [
-    'tracks.video.state',
-    'tracks.audio.state',
-  ]);
+  const [video, audio, screenVideo, isLocal] = useParticipantProperty(
+    sessionId,
+    [
+      'tracks.video.state',
+      'tracks.audio.state',
+      'tracks.screenVideo.state',
+      'local',
+    ],
+  );
 
   const handleTurnOff = useCallback(
-    (device: 'audio' | 'video' = 'video') => {
+    (device: 'audio' | 'video' | 'screen' = 'video') => {
       if (!daily) return;
 
-      const updates =
-        device === 'audio' ? { setAudio: false } : { setVideo: false };
+      if (isLocal && device === 'screen') {
+        // as we can't turn off screen share of local participant
+        // from updateParticipant, we need to use stopScreenShare
+        daily.stopScreenShare();
+        return;
+      }
+
+      const updates: DailyParticipantUpdateOptions =
+        device === 'audio'
+          ? { setAudio: false }
+          : device === 'screen'
+          ? { setScreenShare: false }
+          : { setVideo: false };
       daily.updateParticipant(sessionId, updates);
     },
-    [daily, sessionId],
+    [daily, isLocal, sessionId],
   );
 
   if (!isOwner) return null;
@@ -50,6 +67,13 @@ export function ModerationMenu({ sessionId }: { sessionId: string }) {
       >
         <Icons.micOff className="mr-2 h-4 w-4" />
         <span>Mute microphone</span>
+      </DropdownMenuItem>
+      <DropdownMenuItem
+        disabled={IS_OFF.includes(screenVideo)}
+        onClick={() => handleTurnOff('screen')}
+      >
+        <Icons.screenShareOff className="mr-2 h-4 w-4" />
+        <span>Stop screenshare</span>
       </DropdownMenuItem>
     </>
   );
